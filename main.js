31b9cd21d789781c4b73866afa0ad14015bfa5c9
Mhammad1704 <natgeo1416@hotmail.com>
2019-01-26 21:44:34 +0300

    setup2

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-var table = ['','','',
-             '','','',]
 
-             
+var table = ['x','x','x',
+             'x','x','x',
+             'x','x','x',]
+
+             if(table[0] === table[1] === table[3] ||table[0] === table[4] === table[8]||table[0] === table[3] === table[6] 
+                ||table[1] === table[4] === table[7]||table[2] === table[5] === table[8]||table[3] === table[4] === table[5]
+                ||table[6] === table[7] === table[8]||table[2] === table[4] === table[6])
+                {
+                    console.log('win')
+                }
+                else {
+                    console.log('it work')
+                }
