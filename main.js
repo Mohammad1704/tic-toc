a90330ae6e93160cb62aec3fd241b89c61adfe19
Mhammad1704 <natgeo1416@hotmail.com>
2019-01-27 14:43:59 +0300

    checkResult is working

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ var table = ['','','',
              '','','',
              '','','',]
 
+             // create an object
+             // that has the cell id as the key
+             // and the value for the key as the value in the cell on the screen
+
 var currentUser = 'X'
 // FIND WINNER OF GAME
 var checkResult = function(){
     
-    if(table[0] === table[1] === table[3] ||table[0] === table[4] === table[8]||table[0] === table[3] === table[6] 
-    ||table[1] === table[4] === table[7]||table[2] === table[5] === table[8]||table[3] === table[4] === table[5]
-    ||table[6] === table[7] === table[8]||table[2] === table[4] === table[6])
+    
+  if((table[0] === table[1]) && (table[0] === table[2]) 
+  ||(table[0] === table[4]) && (table[0] === table[8])
+  ||(table[0] === table[3]) && (table[0] === table[6]) 
+    ||(table[1] === table[4]) && (table[1] === table[7])
+    ||(table[2] === table[5]) && (table[2] === table[8])
+    ||(table[3] === table[4]) && (table[3] === table[5])
+    ||(table[6] === table[7]) && (table[6] === table[8])
+    ||(table[2] === table[4]) && (table[2] === table[6]))
     {
         console.log('win')
     }
