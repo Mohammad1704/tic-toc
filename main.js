bbcd872966a9d4a0cd3da8c14db555eec61a837b
Mhammad1704 <natgeo1416@hotmail.com>
2019-01-27 09:26:21 +0300

    start the function

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var table = ['x','x','x',
              'x','x','x',
              'x','x','x',]
 
+
+             var checkResult = function(){
              if(table[0] === table[1] === table[3] ||table[0] === table[4] === table[8]||table[0] === table[3] === table[6] 
                 ||table[1] === table[4] === table[7]||table[2] === table[5] === table[8]||table[3] === table[4] === table[5]
                 ||table[6] === table[7] === table[8]||table[2] === table[4] === table[6])
@@ -12,3 +14,4 @@ var table = ['x','x','x',
                 else {
                     console.log('it work')
                 }
+            }
